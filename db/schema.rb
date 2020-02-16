65da1c8aa02cb6f2507b8f41bfe46edeb2b5a057
kazu <whiterose4429@gmail.com>
2020-02-16 15:18:40 +0900

    基本機能まで完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_042903) do
+ActiveRecord::Schema.define(version: 2020_02_15_081046) do
+
+  create_table "sureds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "editer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
